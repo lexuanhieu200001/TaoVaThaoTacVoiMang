4a79657aa4303832d11da615424b8bd692b0c715
LEXUANHIEU <lexuanhieu200001@gmail.com>
2023-11-20 14:51:47 +0700

    banmot

---
+++ TaovaThaoTacvsMang.js
@@ -0,0 +1,19 @@
+/*let x= 0;
+// cách khai báo biến cho mảng
+let array = Array();
+function add1(){
+    array[x] = document.getElementById('value1').value;
+    alert('element:' + array[x] + 'add' + x );
+    x++
+    document.getElementById('value1').value= "";
+}
+function display1(){
+    let e = "<hr/>";
+    for (let i = 0; i < array.length; i++) {
+        e += "element" + i + " = " + array[i] + "<br/>";
+    }
+    document.getElementById('result').innerHTML= e;
+}*/
+for (let i = 0; i < 4; i++) {
+    document.write( ('*')+ '<br>')
+}
